a7c1dc57026a9da7fc535d0b404bf981b4885beb
layonthehorn <47235696+layonthehorn@users.noreply.github.com>
2019-02-01 18:18:11 -0800

    redundent file

--- randomsong.fish
+++
@@ -1,9 +0,0 @@
-function randomsong
-
-	set songlist (find ~/Music | grep .mp3)
-	set numsongs (count $songlist)
-	set songchoice (random 1 $numsongs)
-		
-	rhythmbox 2> /dev/null $songlist[$songchoice] &
-
-end
